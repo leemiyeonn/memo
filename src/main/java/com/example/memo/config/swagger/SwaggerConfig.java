f51703d63143633ff825611ae738f01eaa3aa7e7
leemiyeon <lemingul7@gmail.com>
2024-07-29 18:22:56 +0900

    feat: swagger 추가

---
+++ src/main/java/com/example/memo/config/swagger/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.example.memo.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    // http://localhost:8080/swagger-ui/index.html#/
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("memo") // API의 제목
+                .description("simple-memo") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
+    }
+}
